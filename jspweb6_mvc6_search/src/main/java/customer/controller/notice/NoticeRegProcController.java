c23cc9cebf75b204d82fa3e991983328017e6868
Jaehwan sin <fate427@naver.com>
2023-07-10 12:36:11 +0900

    230710 SEARCH 기능 추가

---
+++ jspweb6_mvc6_search/src/main/java/customer/controller/notice/NoticeRegProcController.java
@@ -0,0 +1,31 @@
+package customer.controller.notice;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import customer.contoller.Controller;
+import customer.dao.NoticeDao;
+import customer.vo.Notice;
+
+public class NoticeRegProcController implements Controller {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("NoticeRegProcController");
+		
+		request.setCharacterEncoding("UTF-8");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		
+		Notice n = new Notice();
+		n.setTitle(title);
+		n.setContent(content);
+		
+		NoticeDao dao = new NoticeDao();
+		dao.write(n);
+		
+		/* 목록으로 이동 */
+		response.sendRedirect("notice.jsp");
+		// request.getRequestDispatcher("noticeReg.jsp").forward(request,response);
+
+	}
+}
